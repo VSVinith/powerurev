6f03e9631630f2fc1b05d63eb3b65ac234aff2aa
VSVinith <vinithvuppula@gmail.com>
2021-11-26 20:56:59 +0530

    Policies_styling

--- src/Header/index.js
+++ src/Header/index.js
@@ -1,48 +1,50 @@
 // import * as ReactBootStrap from 'react-bootstrap';
 import { Navbar, NavDropdown, Container } from 'react-bootstrap' //import Nav if u use
+// import Link from "react-router-dom"
 import './index.css'
 import image from '../Assets/ev-logo.png'
 
 const Header = () => (
-  <>  <div className="navbar">
-      <div className="power-navbar">       
-          <div>
-          <img alt="logo" className="logo" src={image}/>         
-          </div>
-          <ul className="nav-list nav-list-1">
-          
+  <>
+    <div className="navbar">
+      <div className="power-navbar">
+        <div>
+          <a href="/">
+            <img alt="logo" className="logo" src={image} />
+          </a>
+        </div>
+        <ul className="nav-list nav-list-1">
           <li className="list-item-1"><a className="list-item" href="/">Home</a></li>
           <li className="list-item-1"><a className="list-item" href="/#aboutus">About Us</a></li>
           <li className="list-item-1"><a className="list-item" href="/#whyus">Why Choose Us</a></li>
           <li className="list-item-1"><a className="list-item" href="/#howitworks">How it works</a></li>
-          <li className="list-item-1"><a  href="/#form"><button className="form-btn">Get A Quote</button></a></li>
-  
+          <li className="list-item-1"><a href="/#form"><button className="form-btn">Get A Quote</button></a></li>
         </ul>
       </div>
-      </div>
-      <div className="mobile-nav">
+    </div>
+    <div className="mobile-nav">
       <Navbar className="mobile-nav-padding" bg="dark" variant="dark" expand="lg">
         <Container className="mobile-nav-container logo-position">
-          <Navbar.Brand href="#home"><img alt="logo" className="logo" src={image}/></Navbar.Brand>
+          <Navbar.Brand href="#home"><img alt="logo" className="logo" src={image} /></Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             {/* <Nav className="me-auto">
               <NavDropdown title="Menu" id="basic-nav-dropdown"> */}
-                <NavDropdown.Item className="mobile-list-item" href="/">Home</NavDropdown.Item>
-                <NavDropdown.Item className="mobile-list-item" href="/#aboutus">About Us</NavDropdown.Item>
-                <NavDropdown.Item className="mobile-list-item" href="/#whyus">Why Choose Us</NavDropdown.Item>
-                <NavDropdown.Item className="mobile-list-item" href="/#howitworks">How it works</NavDropdown.Item>
-                <NavDropdown.Item className="mobile-list-item" href="/#howitworks">Register</NavDropdown.Item>
-                <NavDropdown.Divider />
-              {/* </NavDropdown> */}
+            <NavDropdown.Item className="mobile-list-item" href="/">Home</NavDropdown.Item>
+            <NavDropdown.Item className="mobile-list-item" href="/#aboutus">About Us</NavDropdown.Item>
+            <NavDropdown.Item className="mobile-list-item" href="/#whyus">Why Choose Us</NavDropdown.Item>
+            <NavDropdown.Item className="mobile-list-item" href="/#howitworks">How it works</NavDropdown.Item>
+            <NavDropdown.Item className="mobile-list-item" href="/#howitworks">Register</NavDropdown.Item>
+            <NavDropdown.Divider />
+            {/* </NavDropdown> */}
             {/* </Nav> */}
           </Navbar.Collapse>
         </Container>
       </Navbar>
-      </div>
+    </div>
 
-</>
-    
+  </>
+
 )
 
 export default Header
--- src/TermsAndConditions/index.js
+++ src/TermsAndConditions/index.js
@@ -6,16 +6,16 @@ const TermsAndConditions = () => (
     <>
         <Header />
         <div className="terms-and-conditions-container">
-            <h1 className="t-and-c-heading">TERMS & CONDITIONS</h1>
-            <h1>Disclaimer</h1>
+            <h1 className="t-and-c-heading main-heading text-center">TERMS & CONDITIONS</h1>
+            <h1 className="t-and-c-heading">Disclaimer</h1>
             <p className="t-and-c-description">By becoming a customer of powerurev.com services you acknowledge and agree to be bound by all of the following terms and conditions (“Terms”), our Privacy Policy, Disclaimer, and any additional terms that may be applied to the powerurev.com card and its facilities from time to time (collectively, the “Agreement”). If you do not agree to be bound by this Agreement, do not register for our services. Powerurev.com (“The Company”) who operates the services reserves the right to modify or terminate the services at any time and in any manner, in whole or in part. You agree that you will review these Terms and conditions periodically and that you are bound by this Agreement and any modifications we may make to it from time to time.</p>
-            <h1>Terms</h1>
+            <h1 className="t-and-c-heading">Terms</h1>
             <p className="t-and-c-description">Powerurev.com collects an upfront amount of Rs.500/- as visiting charges from the customer.
                 Out of which Rs.200/- would be refundable in case the customer chooses not to opt for powerurev.com services. If the customer opts for our services the amount of Rs.500/- will be adjusted against the amount quoted for the project.
             </p>
             <p className="t-and-c-description">Once the customer registers with Powerurev.com, our customer support team will schedule an appointment at his/her preferred time, if in case the customer wants to reschedule the appointment he/she must inform our customer support team at least two hours prior to the scheduled appointment time. If the customer fails to be available at the location during the scheduled appointment time and if the customer opts for a reschedule, there may be additional charges applicable.</p>
             <p className="t-and-c-description">Once the customer agrees to the quotation provided by powerurev.com the customer must pay 50% payment upfront to start the project. The Remaining amount should be paid post project completion.</p>
-            <h1>Conditions</h1>
+            <h1 className="t-and-c-heading">Conditions</h1>
             <p className="t-and-c-description">It is the customer’s responsibility to take all the necessary permissions required where ever applicable, powerurev.com will not interfere in seeking any kind of permissions from the responsible parties.</p>
             <p className="t-and-c-description">All the products used in the project would only be procured by powerurev.com and the customer has no rights to interfere in the brands or the quality of the products used.</p>
             <p className="t-and-c-description">Powerurev.com does not provide any additional warranty on the products used in the project. However, the brand warranty holds good.</p>
